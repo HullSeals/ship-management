cd9841a89c4f2613511fe86b37e5e9bdafcd3f82
Rixxan <rixxan@hullseals.space>
2020-07-03 13:07:56 -0400

    Update and Condense Scripts

--- seal-fleet.php
+++ seal-fleet.php
@@ -22,39 +22,10 @@ while ($shipclass = $res->fetch_assoc()) {
 <meta content="The Seal Fleets" name="description">
 <title>The Seal Fleets | The Hull Seals</title>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<link href="../../styles.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="//cdnpub.websitepolicies.com/lib/cookieconsent/1.0.2/cookieconsent.min.css" />
-<script src="https://cdnpub.websitepolicies.com/lib/cookieconsent/1.0.2/cookieconsent.min.js" integrity="sha384-gNaqAsLHf4qf+H76HtN+K++WIcDxMT8yQ3VSiYcRjmkwUKZeHXAqppXDBUtja174" crossorigin="anonymous"></script>
-<script>
-        window.addEventListener("load", function() {
-            window.wpcc.init({
-                "colors": {
-                    "popup": {
-                        "background": "#222222",
-                        "text": "#ffffff",
-                        "border": "#bd9851"
-                    },
-                    "button": {
-                        "background": "#bd9851",
-                        "text": "#000000"
-                    }
-                },
-                "border": "thin",
-                "corners": "small",
-                "padding": "small",
-                "margin": "small",
-                "transparency": "25",
-                "fontsize": "small",
-                "content": {
-                    "href": "https://hullseals.space/knowledge/books/important-information/page/cookie-policy"
-                }
-            })
-        });
-    </script>
+<link rel="stylesheet" type="text/css" href="https://hullseals.space/assets/css/allPages.css" />
+<script src="https://hullseals.space/assets/javascript/allPages.js" integrity="sha384-PsQdnKGi+BdHoxLI6v+pi6WowfGtnraU6GlDD4Uh5Qw2ZFiDD4eWNTNG9+bHL3kf" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
   </head>
   <body>
   <div id="home">
